77f25bc0dcc54402fb5d112b152f9f1800b8a6e9
FediMechergui <fedimechergui03@gmail.com>
2024-06-26 14:41:45 +0100

    analytics imlemented

---
+++ Omnilink/src/analytics.js
@@ -0,0 +1,6 @@
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = "G-JWF01Z0Q09"; 
+ReactGA.initialize(TRACKING_ID);
+
+export default ReactGA;
--- Omnilink/src/main.jsx
+++ Omnilink/src/main.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Main from './Pages/Main.jsx';
 import ContactUs from './Pages/ContactUs.jsx';
+import ReactGA from './analytics.js'; 
+
+
+function TrackPageViews() {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <TrackPageViews /> 
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/contact" element={<ContactUs />} />
